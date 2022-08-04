04f4a8123b9f436632f34610e637eae48272a28c
rsquaredin <rsquaredin@gmail.com>
2022-08-04 13:07:04 +0530

    chore: preview

---
+++ R/preview.R
@@ -0,0 +1,26 @@
+#' @export
+#' @import htmltools shiny
+preview_standby <- function(standby, bg_color = "black") {
+
+  if(missing(standby))
+    stop("Missing standby", call. = FALSE)
+
+  htmltools::browsable(
+    tagList(
+      tags$head(
+        use_three(),
+        tags$style(
+          paste0("body{background: ", bg_color,";}")
+        )
+      ),
+      div(
+        style = "position: relative;height:90vh;",
+        div(
+          style = "position: absolute;top:50%;left:50%;",
+          standby
+        )
+      )
+    )
+  )
+
+}
