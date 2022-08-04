5646181f2f7bd845721f603707e5d7252b55011e
rsquaredin <rsquaredin@gmail.com>
2022-08-04 19:59:58 +0530

    chore: try to center loader

--- R/standby.R
+++ R/standby.R
@@ -1,9 +1,10 @@
 #' @export
-standby <- function(type = "bouncing") {
+standby <- function(ui_element, type = "bouncing") {
   tags$div(
     class = "standby",
     tags$div(
       class = paste0("dot-", type, " wait")
-    )
+    ),
+    ui_element
   )
 }
--- inst/script.js
+++ inst/script.js
@@ -1,3 +1,24 @@
+function detectLocation() {
+  let byHeight = $('.wait').parent().height();
+  let byWidth = $('.wait').parent().width();
+  let loaderWidth = $('.wait').width();
+  let loaderHeight = $('.wait').height();
+  $('.wait').css('left', (byWidth - loaderWidth) / 2);
+  $('.wait').css('top', (byHeight - loaderHeight) / 2);
+}
+
+// $(document).ready(function () {
+//   let byHeight = $('.wait').parent().height();
+//   let byWidth = $('.wait').parent().width();
+//   let loaderWidth = $('.wait').width();
+//   let loaderHeight = $('.wait').height();
+//   $('.wait').css('left', (byWidth - loaderWidth) / 2);
+//   $('.wait').css('top', (byHeight - loaderHeight) / 2);
+// });
+
+$(document).ready(detectLocation);
+$(window).on('resize', detectLocation);
+
 $(document).on('shiny:busy', function (event) {
   let allWait = $('.wait');
   $('.standby').find(allWait).css('visibility', 'visible');
