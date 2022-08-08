284e6e78b125458a6e395ce68ef8d3fcc8bac3a3
rsquaredin <rsquaredin@gmail.com>
2022-08-08 19:14:51 +0530

    fix: dependencies:

--- R/deps.R
+++ R/deps.R
@@ -1,43 +1,54 @@
-#' @export
-useThreeDots <- function() {
+#' @export  
+standby <- function() {
 
-  pkg <- htmltools::htmlDependency(
-    name = "sby-pkg",
-    version = "0.1.0",
-    src = "",
-    script = list(src = "script.js", defer = NA),
-    stylesheet = "three-dots.css",
-    package = "standby"
-  )
-
-  sby_style <- htmltools::htmlDependency(
+  htmltools::htmlDependency(
     name = "sby-css",
     version = "0.1.0",
     src = "",
+    script = list(src = "script.js", defer = NA),
     stylesheet = "style.css",
     package = "standby"
   )
 
-  htmltools::tagList(pkg, sby_style)
+}
+
+#' @export
+useThreeDots <- function() {
+
+  dots <- htmltools::htmlDependency(
+    name = "sby-pkg",
+    version = "0.1.0",
+    src = "",
+    stylesheet = "three-dots.css",
+    package = "standby"
+  )
+
+  sby <- standby()
+
+  htmltools::tagList(dots, sby)
 
 }
 
 #' @export
 useSpinkit <- function() {
 
-  htmltools::htmlDependency(
+  spin <- htmltools::htmlDependency(
     name = "sby-spinkit",
     version = "0.1.0",
     src = "",
     stylesheet = "spinkit.min.css",
     package = "standby"
   )
+
+  sby <- standby()
+
+  htmltools::tagList(spin, sby)
 }
 
 #' @export
 useVizLoad <- function() {
 
-  htmltools::htmlDependency(
+  viz <- htmltools::htmlDependency(
     name = "sby-vizload",
     version = "0.1.0",
     src = "",
@@ -45,31 +56,40 @@ useVizLoad <- function() {
     stylesheet = "ridics.css",
     package = "standby"
   )
+
+  sby <- standby()
+
+  htmltools::tagList(viz, sby)
 }
 
 #' @export
 useSpinners <- function() {
 
-  htmltools::htmlDependency(
+  spinner <- htmltools::htmlDependency(
     name = "sby-spinners",
     version = "0.1.0",
     src = "",
     stylesheet = "spinners.css",
     package = "standby"
   )
+
+  sby <- standby()
+
+  htmltools::tagList(spinner, sby)
 }
 
 #' @export
 useLoaders <- function() {
 
-  htmltools::htmlDependency(
+  loader <- htmltools::htmlDependency(
     name = "sby-loaders",
     version = "0.1.0",
     src = "",
     stylesheet = "loaders.css",
     package = "standby"
   )
-}
 
-# useCssLoaders()
-# useCssSpinners()
+  sby <- standby()
+
+  htmltools::tagList(loader, sby)
+}
