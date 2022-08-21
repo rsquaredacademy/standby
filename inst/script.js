b0077dd15ba4f28dcf88ead13c5487ee4ba2ea51
rsquaredin <rsquaredin@gmail.com>
2022-08-21 11:59:10 +0530

    feat: center of page

--- inst/script.js
+++ inst/script.js
@@ -1,6 +1,8 @@
 $(document).on('shiny:recalculating', function (event) {
   let id = '#' + event.target.id;
   let output = document.querySelector(id);
+  let parent = output.parentElement;
+  parent.classList.add('full-screen');
   let loader = $(output).siblings('.standby-wait')[0];
   output.style.visibility = 'hidden';
   loader.style.visibility = 'visible';
@@ -9,6 +11,8 @@ $(document).on('shiny:recalculating', function (event) {
 $(document).on('shiny:value shiny:error', function (event) {
   let id = '#' + event.target.id;
   let output = document.querySelector(id);
+  let parent = output.parentElement;
+  parent.classList.remove('full-screen');
   let loader = $(output).siblings('.standby-wait')[0];
   output.style.visibility = 'visible';
   loader.style.visibility = 'hidden';
