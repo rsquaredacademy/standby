e0c5492340954e84cf4e6d02d217da84d8d3363f
rsquaredin <rsquaredin@gmail.com>
2023-09-26 18:25:12 +0530

    docs: closes #4

--- R/standby.R
+++ R/standby.R
@@ -1,12 +1,14 @@
 #' \code{standby} package
-#' 
+#'
 #' Loading screens for Shiny.
-#' 
-#' See the README on 
+#'
+#' See the README on
 #' \href{https://github.com/rsquaredacademy/standby}{GitHub}
-#' 
+#'
 #' @import shiny htmltools grDevices
 #' @keywords internal
 #' @docType package
 #' @name standby
+#' @aliases standby-package
+"_PACKAGE"
 NULL
