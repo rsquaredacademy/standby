51df90c33a8f837be1d84b5aca0ed0465dcc4862
rsquaredin <rsquaredin@gmail.com>
2023-09-26 15:00:34 +0530

    fix: output and loader visibility changed for all target ids

--- inst/standby/js/standby.js
+++ inst/standby/js/standby.js
@@ -2,18 +2,24 @@ $(document).on('shiny:recalculating', function (event) {
   let id = '#' + event.target.id;
   let output = document.querySelector(id);
   let parent = output.parentElement;
-  parent.classList.add('full-screen');
-  let loader = $(output).siblings('.standby-wait')[0];
-  output.style.visibility = 'hidden';
-  loader.style.visibility = 'visible';
+  let isParent = parent.classList.contains('standby');
+  if (isParent) {
+    parent.classList.add('full-screen');
+    let loader = $(output).siblings('.standby-wait')[0];
+    output.style.visibility = 'hidden';
+    loader.style.visibility = 'visible';
+  }
 });
 
 $(document).on('shiny:value shiny:error', function (event) {
   let id = '#' + event.target.id;
   let output = document.querySelector(id);
   let parent = output.parentElement;
-  parent.classList.remove('full-screen');
-  let loader = $(output).siblings('.standby-wait')[0];
-  output.style.visibility = 'visible';
-  loader.style.visibility = 'hidden';
+  let isParent = parent.classList.contains('standby');
+  if (isParent) {
+    parent.classList.remove('full-screen');
+    let loader = $(output).siblings('.standby-wait')[0];
+    output.style.visibility = 'visible';
+    loader.style.visibility = 'hidden';
+  }
 });
